11cac30439b80fb850fa5f84518bca9e5e18488a
Leo Ando <ando.reo420@gmail.com>
2021-07-29 21:28:46 +0900

    コメント追記

--- GradientSample/02/js/main.js
+++ GradientSample/02/js/main.js
@@ -7,7 +7,7 @@ function createRadialGradient() {
     var canvas = document.getElementById("canvas");
     var context = canvas.getContext("2d");
     // グラデーションの作成
-    var x1 = 150; y1 = 150, r1 = 50; // 1つ目の円形: 座標(x, y), r: 半径
+    var x1 = 150; y1 = 150, r1 = 50; // 1つ目の円形: 座標(x, y)を中心とした r: 半径の円
     var x2 = 150; y2 = 150, r2 = 100;// 2つ目の円形
     var gradient = context.createRadialGradient(x1, y1, r1, x2, y2, r2);
     gradient.addColorStop(0.3, "#0d47a1"); // 地点, 色指定(RGB)
